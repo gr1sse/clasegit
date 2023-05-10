b944ad986c0987c7f5dc3054fbbb7b1d5f214f2c
gr1sse <das0030045@est.univalle.edu>
2023-05-10 16:00:46 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int result = Sum(2, 3);
+        }
+
+        public int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
